0abb17abdc50281799c09fbb1f38afb4a9ee53e6
Jesse Lovitt <lovi9573@hotmail.com>
2015-12-02 22:04:57 -0700

    working on tensorflow rbm code

--- models/tensorflow/rbm_binary.py
+++ models/tensorflow/rbm_binary.py
@@ -13,31 +13,39 @@ import os
 import sys
 
 import tensorflow.python.platform
-
+import rbmconfig_pb2 as proto
+from google.protobuf.text_format import Merge
 import numpy
 from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
+from weights_to_img import display
+
+config = proto.RbmParameters()
+
+with open(sys.argv[1],'r') as fin:
+  Merge(fin.read(),config)
 
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
-WORK_DIRECTORY = '/home/jlovitt/storage/data/mnist'
+WORK_DIRECTORY = '/home/user/storage/mnist'
 IMAGE_SIZE = 28
 NUM_CHANNELS = 1
 PIXEL_DEPTH = 255
 NUM_LABELS = 10
 VALIDATION_SIZE = 5000  # Size of the validation set.
 SEED = 66478  # Set to None for random seed.
-BATCH_SIZE = 64
+BATCH_SIZE = config.batch_size
 NUM_EPOCHS = 10
 NUM_HIDDEN = 32
 V_DIM = IMAGE_SIZE*IMAGE_SIZE*NUM_CHANNELS
 SAMPLE_HIDDEN = True
 SAMPLE_VISIBLE = True
 N_GIBBS_STEPS = 10
+REPORT=100
 
 
-tf.app.flags.DEFINE_boolean("self_test", False, "True if running a self test.")
-FLAGS = tf.app.flags.FLAGS
+#tf.app.flags.DEFINE_boolean("self_test", False, "True if running a self test.")
+#FLAGS = tf.app.flags.FLAGS
 
 
 def maybe_download(filename):
@@ -64,6 +72,7 @@ def extract_data(filename, num_images):
     data = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.float32)
     data = data / PIXEL_DEPTH
     data = data.reshape(num_images, IMAGE_SIZE*IMAGE_SIZE)
+    print(data.shape[0])
     return data
 
 
@@ -100,56 +109,30 @@ def error_rate(predictions, labels):
 
 
 def main(argv=None):  # pylint: disable=unused-argument
-  if FLAGS.self_test:
-    print('Running self-test.')
-    train_data, train_labels = fake_data(256)
-    validation_data, validation_labels = fake_data(16)
-    test_data, test_labels = fake_data(256)
-    num_epochs = 1
-  else:
-    # Get the data.
-    train_data_filename = maybe_download('train-images-idx3-ubyte.gz')
-    test_data_filename = maybe_download('t10k-images-idx3-ubyte.gz')
+  # Get the data.
+  train_data_filename = maybe_download('train-images-idx3-ubyte.gz')
+  test_data_filename = maybe_download('t10k-images-idx3-ubyte.gz')
 
-    # Extract it into numpy arrays.
-    train_data = extract_data(train_data_filename, 60000)
-    test_data = extract_data(test_data_filename, 10000)
+  # Extract it into numpy arrays.
+  train_data = extract_data(train_data_filename, 60000)
 
-    # Generate a validation set.
-    train_data = train_data[:VALIDATION_SIZE, :]
-    validation_data = train_data[VALIDATION_SIZE:, :]
-    num_epochs = NUM_EPOCHS
+  # Generate a validation set.
+  train_data = train_data[:-VALIDATION_SIZE, :]
+  num_epochs = NUM_EPOCHS
   train_size = train_data.shape[0]
+  print("Training data samples: {}".format(train_size))
 
-  # This is where training samples and labels are fed to the graph.
-  # These placeholder nodes will be fed a batch of training data at each
-  # training step using the {feed_dict} argument to the Run() call below.
-  # TODO(jesse lovitt): Can I modify this in place?
-  visible = tf.placeholder(
-      tf.float32,
-      shape=(BATCH_SIZE,V_DIM))
-  
-  # For the validation and test data, we'll just hold the entire dataset in
-  # one constant node.
-  validation_data_node = tf.constant(validation_data)
-  test_data_node = tf.constant(test_data)
-
-  # The variables below hold all the trainable weights. They are passed an
-  # initial value which will be assigned when when we call:
-  # {tf.initialize_all_variables().run()}
+  #Setup persistent vars
+  visible = tf.placeholder(tf.float32,shape=(BATCH_SIZE,V_DIM))
   weights = tf.Variable(
       tf.truncated_normal([NUM_HIDDEN, V_DIM],
                           stddev=0.1,
                           seed=SEED))
   bias_h = tf.Variable(tf.zeros([NUM_HIDDEN]))
   bias_v = tf.Variable(tf.constant(0.1, shape=[V_DIM]))
-
-
   recon = tf.Variable(tf.zeros([BATCH_SIZE, V_DIM]))
   hidden = tf.Variable(tf.zeros([BATCH_SIZE, NUM_HIDDEN],dtype=tf.float32))
   
-  E_p = tf.Variable(tf.zeros([NUM_HIDDEN,V_DIM]))
-
   def v_h(v, sample=True):
     hidden.assign( tf.matmul(v,weights,transpose_b=True)+bias_h)
     if sample:
@@ -164,15 +147,25 @@ def main(argv=None):  # pylint: disable=unused-argument
       recon.assign( tf.to_float(recon > thresh))
     return recon
 
-  # We will replicate the model structure for the training subgraph, as well
-  # as the evaluation subgraphs, while sharing the trainable parameters.
+  def Energy(v,h):
+    hwvr = tf.reduce_sum(h*tf.matmul(v,weights, transpose_b=True),reduction_indices=1)
+    bh = tf.matmul(h,tf.expand_dims(bias_h,-1))
+    bv = tf.matmul(v,tf.expand_dims(bias_v,-1))
+    return -(hwvr + bh + bv)
+ 
+  def FreeEnergy(v):
+    e = tf.reduce_sum(tf.log(1 + tf.exp( tf.matmul(v,weights,transpose_b=True)+bias_h)),reduction_indices=1)
+    bv = tf.matmul(v,tf.expand_dims(bias_v,-1)) 
+    return -(bv + e)
+
   def model(data, train=False):
     """The Model definition."""
     # Positive Phase
     hidden.assign( v_h(data, SAMPLE_HIDDEN))
     
     # Get positive phase energy
-    E_p = tf.matmul(hidden, data, transpose_a=True)
+    F_p = FreeEnergy(data)
+
     h = hidden
     # Gibbs chain  
     for i in range(N_GIBBS_STEPS-1):
@@ -182,55 +175,48 @@ def main(argv=None):  # pylint: disable=unused-argument
     hidden.assign( v_h(recon, SAMPLE_HIDDEN))
   
     # Get positive phase energy
-    E_n = tf.matmul(hidden,recon, transpose_a=True)
-    
-    return E_p - E_n
+    F_n = FreeEnergy(recon)   
+    return F_p - F_n
 
-  # Training computation: logits + cross-entropy loss.
-  E = model(visible, True)
-  loss = tf.reduce_mean(E)
-
+  # Build computation graph
+  F = model(visible, True)
+  loss = tf.reduce_mean(F)
   
-  # Optimizer: set up a variable that's incremented once per batch and
-  # controls the learning rate decay.
+  # Learning rate scheduling
   batch = tf.Variable(0)
-  # Decay once per epoch, using an exponential schedule starting at 0.01.
   learning_rate = tf.train.exponential_decay(
-      0.01,  # Base learning rate.
+      0.001,  # Base learning rate.
       batch * BATCH_SIZE,  # Current index into the dataset.
       train_size,  # Decay step.
       0.95,  # Decay rate.
       staircase=True)
-  # Use simple momentum for the optimization.
+ 
+  # Optimization
   optimizer = tf.train.MomentumOptimizer(learning_rate,
                                          0.9).minimize(loss,
+                                                       var_list=[weights,bias_h,bias_v],
                                                        global_step=batch)
 
 
   # Create a local session to run this computation.
   with tf.Session() as s:
-    # Run all the initializers to prepare the trainable parameters.
     tf.initialize_all_variables().run()
     print('Initialized!')
-    # Loop through training steps.
     for step in xrange(num_epochs * train_size // BATCH_SIZE):
-      # Compute the offset of the current minibatch in the data.
-      # Note that we could use better randomization across epochs.
       offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
       batch_data = train_data[offset:(offset + BATCH_SIZE), :]
-      # This dictionary maps the batch data (as a numpy array) to the
-      # node in the graph is should be fed to.
       feed_dict = {visible: batch_data}
-      # Run the graph and fetch some of the nodes.
       _, l, lr = s.run(
           [optimizer, loss, learning_rate],
           feed_dict=feed_dict)
-      if step % 100 == 0:
+      if step % REPORT == 0:
         print('Epoch %.2f' % (float(step) * BATCH_SIZE / train_size))
         print('Minibatch loss: %.3f, learning rate: %.6f' % (l, lr))
+        w = s.run(weights).reshape([NUM_HIDDEN,28,28,1])
+        b_v = s.run(bias_v).reshape([1,28,28,1])
+        display(numpy.append(w, b_v, 0))
         sys.stdout.flush()
-    # Finally print the result!
-
 
 if __name__ == '__main__':
-  tf.app.run()
+  #tf.app.run()
+  main()

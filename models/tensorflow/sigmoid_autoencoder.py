9ef71163a5f652e17e85d495e0349120af73fc27
jlovitt <jlovitt@localhost.localdomain>
2015-12-05 13:35:44 -0700

    updates to autoencoder and prototxt

--- models/tensorflow/sigmoid_autoencoder.py
+++ models/tensorflow/sigmoid_autoencoder.py
@@ -20,6 +20,8 @@ from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 from weights_to_img import display
+import math
+import matplotlib.pyplot as plt
 
 config = proto.RbmParameters()
 
@@ -33,7 +35,7 @@ NUM_CHANNELS = 1
 PIXEL_DEPTH = 255
 NUM_LABELS = 10
 VALIDATION_SIZE = 5000  # Size of the validation set.
-SEED = 66478  # Set to None for random seed.
+SEED = None  # Set to None for random seed.
 BATCH_SIZE = config.batch_size
 NUM_EPOCHS = config.epochs
 NUM_HIDDEN = config.num_hidden
@@ -49,7 +51,8 @@ SPARSITY = config.use_sparsity_target
 SPARSITY_TARGET = config.sparsity_target
 SPARSITY_LR = config.sparsity_learning_rate
 SPARSITY_DECAY = config.sparsity_decay
-LAMBDA = 0.0001
+LAMBDA = config.weight_decay_coef
+
 
 
 #tf.app.flags.DEFINE_boolean("self_test", False, "True if running a self test.")
@@ -134,41 +137,51 @@ def main(argv=None):  # pylint: disable=unused-argument
   visible = tf.placeholder(tf.float32,shape=(BATCH_SIZE,V_DIM))
   weights = tf.Variable(
       tf.truncated_normal([NUM_HIDDEN, V_DIM],
-                          stddev=0.1,
+                          stddev=0.01,
                           seed=SEED))
   bias_h = tf.Variable(tf.zeros([NUM_HIDDEN]))
   bias_v = tf.Variable(tf.zeros([V_DIM]))
-  sparsity = tf.Variable(tf.fill([1],SPARSITY_TARGET))
 
   def v_h(v):
     h = tf.sigmoid(tf.matmul(v,weights,transpose_b=True) +bias_h)
     return h
   
   def h_r(h):
-    r = tf.sigmoid(tf.matmul(h,weights) + bias_v)
+    r = tf.sigmoid(tf.matmul(h,weights) + bias_v )
     return r
 
   def kl(p, p_hat):
     a = p*tf.log(tf.div(p,p_hat))
     b = (1-p)*tf.log(tf.div((1-p),(1-p_hat)))
-    return tf.reduce_sum(a+b)
+    return tf.reduce_mean(a+b)
   
-  def weight_decay(W,b):
-    return tf.reduce_sum(tf.pow(W,2)) + tf.reduce_sum(tf.pow(b,2))
+  def cross_entropy(v,r):
+    e = 0.00001
+    return -tf.reduce_mean((v*tf.log(r+e) + (1-v)*tf.log(1-r-e)))
+  
+  def weight_decay(W,b_v,b_h):
+    return tf.reduce_sum(tf.pow(W,2)) + tf.reduce_sum(tf.pow(b_h,2))+tf.reduce_sum(tf.pow(b_v,2))
 
-  def model(data, train=False):
+  def bernoulli(shape, thresh):
+    n = tf.random_uniform(shape)
+    return tf.to_float(n > thresh)
+
+  def model(d, train=False):
     """The Model definition."""
+    data = d*bernoulli(tf.shape(d),0.5)
     h = v_h(data) 
     r = h_r(h)
    
-    return r - data, h, r
+    return data, h, r
 
   # Build computation graph
-  diff, h, r = model(visible, True)
+  v, h, r = model(visible, True)
   print("Using sparsity target: {}".format(SPARSITY_TARGET))
-  loss = tf.reduce_mean(tf.pow(diff,2)) +\
-         SPARSITY_LR*kl(SPARSITY_TARGET,tf.reduce_mean(h,0))+\
-         LAMBDA*weight_decay(weights,bias_h) 
+  err_cost = cross_entropy(v,r)
+  sparsity = SPARSITY_LR*kl(SPARSITY_TARGET,tf.reduce_mean(h,0))
+  weight_cost = LAMBDA*weight_decay(weights,bias_v,bias_h)
+  loss = err_cost + sparsity + weight_cost
+          
   
   # Learning rate scheduling
   batch = tf.Variable(0)
@@ -176,7 +189,7 @@ def main(argv=None):  # pylint: disable=unused-argument
       LR,  # Base learning rate.
       batch * BATCH_SIZE,  # Current index into the dataset.
       train_size,  # Decay step.
-      0.99,  # Decay rate.
+      0.999,  # Decay rate.
       staircase=True)
  
   # Optimization
@@ -185,11 +198,15 @@ def main(argv=None):  # pylint: disable=unused-argument
                                                        var_list=[weights, bias_h,bias_v],
                                                        global_step=batch)
 
-  display_progress = False
+
   # Create a local session to run this computation.
   with tf.Session() as s:
     tf.initialize_all_variables().run()
     print('Initialized!')
+    costs = {"err":[],
+             "sparsity": [],
+             "weight": []}
+    display_step = 1
     for step in xrange(num_epochs * train_size // BATCH_SIZE):
       offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
       batch_data = train_data[offset:(offset + BATCH_SIZE), :]
@@ -197,25 +214,35 @@ def main(argv=None):  # pylint: disable=unused-argument
       _, l, lr = s.run(
           [optimizer, loss, learning_rate],
           feed_dict=feed_dict)
-      if numpy.max(numpy.abs(s.run(weights))) > 1000:
-        display_progress = True
+      e = s.run(err_cost,feed_dict=feed_dict)
+      sp = s.run(sparsity,feed_dict=feed_dict)
+      w = s.run(weight_cost,feed_dict=feed_dict)
+      costs["err"].append(e)
+      costs["sparsity"].append(sp)
+      costs["weight"].append(w)
       if step % REPORT == 0:
         print('Epoch %.2f' % (float(step) * BATCH_SIZE / train_size))
-        print('Minibatch loss: %.3f, learning rate: %.6f' % (l, lr))
-      if display_progress == True or (step+1)%(50*REPORT) == 0:
+        print('Minibatch loss:(err,sparsity,weight) %.3f: %3f,%3f,%3f   learning rate: %.6f' % (l, e, sp, w, lr))
+      if step == display_step or (step+1)%(20*train_size // BATCH_SIZE) == 0:
         #display(s.run(visiblevar).reshape([BATCH_SIZE,28,28,1]))
-        ht = s.run(tf.reduce_mean(h,0),feed_dict=feed_dict).reshape([NUM_HIDDEN,1,1,1])
+        ht = s.run(h,feed_dict=feed_dict).reshape([1,BATCH_SIZE,NUM_HIDDEN,1])
         display(ht)
         if SPARSITY:
-          bh = s.run(sparsity,feed_dict=feed_dict).reshape([NUM_HIDDEN,1,1,1])
+          bh = s.run(tf.reduce_mean(h,0),feed_dict=feed_dict).reshape([NUM_HIDDEN,1,1,1])
           display(bh)
-        v = batch_data.reshape([BATCH_SIZE,28,28,1])
+        vt = s.run(v,feed_dict=feed_dict).reshape([BATCH_SIZE,28,28,1])
         rt = s.run(r,feed_dict=feed_dict).reshape([BATCH_SIZE,28,28,1])
-        display(numpy.append(v,rt,0))
+        display(numpy.append(vt,rt,0))
         w = s.run(weights,feed_dict=feed_dict).reshape([NUM_HIDDEN,28,28,1])
         b_v = s.run(bias_v,feed_dict=feed_dict).reshape([1,28,28,1])
         display(numpy.append(w, b_v, 0))
+        plt.plot(costs["err"],'r', costs["sparsity"],'b', costs["weight"],'k')
+        x0,x1,y0,y1 = plt.axis()
+        plt.axis((x0,x1,y0,0.2*y1))
+        plt.show()
         sys.stdout.flush()
+        display_step += int(REPORT*math.log(4*step+5))
+        
 
 if __name__ == '__main__':
   #tf.app.run()
